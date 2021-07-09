9d4837b5bbde34ec8901851bd88bc03549b9ed50
Artextrim5@gmail.com <Artextrim5@gmail.com>
2021-07-09 22:38:18 +0300

    Сделал загрузочный экран

---
+++ BackYardGarden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    int currentBuildIndex;
+    [SerializeField] float loadDelay = 3f;
+    [SerializeField] float dethDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentBuildIndex == 0)
+        {
+            StartCoroutine(LoadSceneDelay());
+        }
+    }
+
+   
+    IEnumerator LoadSceneDelay()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        LoudNextScene();
+    }
+
+    public void LoudNextScene()
+    {
+        SceneManager.LoadScene(currentBuildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+   public void GameOverScene()
+    {
+        StartCoroutine(LoadDethDelay());
+    }
+
+    IEnumerator LoadDethDelay()
+    {
+        yield return new WaitForSeconds(dethDelay);
+        SceneManager.LoadScene(1);
+    }
+
+}
