1ae1ddef082353ac43bf2e9e8de690facd95f0a3
artextrim5 <artextrim5@yandex.ru>
2021-07-20 20:08:50 +0300

    сделал выбор защитной постройки

--- BackYardGarden/Assets/Scripts/DefenderSpawner.cs
+++ BackYardGarden/Assets/Scripts/DefenderSpawner.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject defender;
+    Defender defender;
 
     private void OnMouseDown()
     {
         SpawnDefender(GetSuareCliked());
     }
 
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
     private Vector2 GetSuareCliked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGreed(worldPos);
+        return gridPos;
     }
 
-    private void SpawnDefender(Vector2 worldPos)
+    private Vector2 SnapToGreed(Vector2 rawWorldPos)
     {
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+        Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
     }
 
 
